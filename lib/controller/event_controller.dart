885ae08ab05697b0ce217a18c53c7f2d895a0e31
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-24 16:42:55 +0530

    Basic

--- lib/controller/event_controller.dart
+++ lib/controller/event_controller.dart
@@ -6,7 +6,11 @@ class EventController extends GetxController {
 
   @override
   void onReady() {
-    
+    upcomingEvent.add(
+      Event(title: "Test app", location: "Bennett University", description: "fifbvfwefb fweukfbwekjfbe ejfnfsf", eventDate: DateTime.now(), availableSeats: 20, cost: 300, eventId: "ihbviwbfw4235aas", domain: "Web Development", imageUrl: 'https://images.unsplash.com/photo-1663841349085-593c41127967?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2474&q=80', maximumMembers: 5, minimumMembers: 1,)
+    );
+
     super.onReady();
   }
+  
 }
--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -1,12 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:techathon/controller/event_controller.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    EventController eventController = Get.put(EventController());
+    String type = eventController.upcomingEvent[0].maximumMembers == 1
+        ? "Solo"
+        : eventController.upcomingEvent[0].minimumMembers > 1
+            ? "Team"
+            : "Solo&Team";
     return Scaffold(
-  //  body: SafeArea(child: ),
+      body: eventController.upcomingEvent.isEmpty
+          ? const FlutterLogo()
+          : Column(
+              children: [
+                Container(
+                  height: 276.h,
+                  width: 375.w,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.fitWidth,
+                          image: NetworkImage(
+                            eventController.upcomingEvent[0].imageUrl,
+                          ))),
+                  child: SafeArea(
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                                onPressed: null,
+                                icon: Icon(
+                                  Icons.arrow_back_ios_new_rounded,
+                                  color: Colors.white,
+                                  size: 30.sp,
+                                )),
+                            IconButton(
+                                onPressed: null,
+                                icon: Icon(
+                                  Icons.ios_share_outlined,
+                                  color: Colors.white,
+                                  size: 30.sp,
+                                )),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Pill(type: type),
+                            
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
     );
+  }
+}
+
+class Pill extends StatelessWidget {
+  const Pill({
+    Key? key,
+    required this.type,
+  }) : super(key: key);
+
+  final String type;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 86.w,
+        height: 17.h,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.r),
+            color: const Color(0xff233C7B)),
+        child: Text(type,
+        style: TextStyle(color: Colors.white
+        ,fontSize: 12.sp,
+        fontFamily: GoogleFonts.inter().fontFamily
+        ),
+        ));
   }
 }
