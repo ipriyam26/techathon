69d1d5c408369c5ccec870abb009f607809633a5
Sahil Sharma <74351253+ys1113457623@users.noreply.github.com>
2022-09-25 00:06:50 +0530

    event_card

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:techathon/view/screen/event.dart';
+import 'package:techathon/widgets/event_container.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,23 +15,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(375, 812),
+        designSize: const Size(360, 800),
         minTextAdapt: true,
         splitScreenMode: true,
         builder: (context, a) {
           return GetMaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
-            theme: ThemeData(
-             
-              primarySwatch: Colors.blue,
-            ),
-            home: EventScreen()
-          );
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              home: Container(
+                  color: Colors.black,
+                  child: CarouselSlider(
+                    options: CarouselOptions(aspectRatio: 1.2),
+                    items: const [
+                      EventContainer(
+                        date: '2022-09-29',
+                        name: 'Tech Fest',
+                        paid: true,
+                        teamCategory: 'Team & Solo',
+                        location: 'Bennett University',
+                        participantLength: 20,
+                        img_url: 'https://i.imgur.com/CzXTtJV.jpg',
+                      ),
+                      EventContainer(
+                        date: '2022-09-29',
+                        name: 'Tech Fest',
+                        paid: true,
+                        teamCategory: 'Team & Solo',
+                        location: 'Bennett University',
+                        participantLength: 20,
+                        img_url: 'https://i.imgur.com/CzXTtJV.jpg',
+                      ),
+                      EventContainer(
+                        date: '2022-09-29',
+                        name: 'Tech Fest',
+                        paid: true,
+                        teamCategory: 'Team & Solo',
+                        location: 'Bennett University',
+                        participantLength: 20,
+                        img_url: 'https://i.imgur.com/CzXTtJV.jpg',
+                      ),
+                      EventContainer(
+                        date: '2022-09-29',
+                        name: 'Tech Fest',
+                        paid: true,
+                        teamCategory: 'Team & Solo',
+                        location: 'Bennett University',
+                        participantLength: 20,
+                        img_url: 'https://i.imgur.com/CzXTtJV.jpg',
+                      )
+                    ],
+                  )));
         });
   }
 }
-
-
-
-
