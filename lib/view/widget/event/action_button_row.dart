4c4e81ff1bccf26dedb0b810eebd59fc3778bb57
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-09-25 15:13:19 +0530

    home screen header done

--- lib/view/widget/event/action_button_row.dart
+++ lib/view/widget/event/action_button_row.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:techathon/controller/event_controller.dart';
 class ActionButtonRow extends StatelessWidget {
   const ActionButtonRow({
     Key? key,
