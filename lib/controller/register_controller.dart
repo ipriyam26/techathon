1bd9cb22ca4b860607940d69c24c7b1704d84b05
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-25 10:58:13 +0530

    Register Pick Image Backend

--- lib/controller/register_controller.dart
+++ lib/controller/register_controller.dart
@@ -1,15 +1,20 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart' as Imp;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:techathon/model/register.dart';
+import 'package:http/http.dart' as http;
 
 class RegisterController extends GetxController {
   late Register register;
   RxString teamName = "".obs;
-  RxString imageUrl = "".obs;
+  RxString imagePath = "".obs;
   RxString name = "".obs;
   RxString phoneNumber = "".obs;
   RxString email = "".obs;
   RxInt age = 0.obs;
+  RxString imageUrl = "".obs;
   ApplyingWith applyingWith = ApplyingWith.solo;
   Gender gender = Gender.male;
   late Education education;
@@ -28,6 +33,47 @@ class RegisterController extends GetxController {
     this.eventInfo = eventInfo;
   }
 
+  void getFromGallery() async {
+    // final ImagePicker picker = ImagePicker();
+    final Imp.ImagePicker picker = Imp.ImagePicker();
+    final Imp.XFile? image =
+        await picker.pickImage(source: Imp.ImageSource.gallery);
+    if (image == null) return;
+    final imagePathLocal = image.path;
+    imagePath.value = imagePathLocal;
+    update();
+  }
+
+  void getFromCamera() async {
+    // final ImagePicker picker = ImagePicker();
+    final Imp.ImagePicker picker = Imp.ImagePicker();
+    final Imp.XFile? image =
+        await picker.pickImage(source: Imp.ImageSource.camera);
+    if (image == null) return;
+    final imagePathLocal = image.path;
+    imagePath.value = imagePathLocal;
+    update();
+  }
+
+  uploadToImgur() async {
+    final pngByteData = await File(imagePath.value).readAsBytes();
+    var headers = {'Authorization': 'Client-ID {{a83359f69b06947}}'};
+    var request = http.MultipartRequest(
+        'POST', Uri.parse('https://api.imgur.com/3/image'));
+    request.fields.addAll({'image': '$pngByteData'});
+    request.headers.addAll(headers);
+    http.StreamedResponse response = await request.send();
+    if (response.statusCode == 200) {
+      if (kDebugMode) {
+        print(await response.stream.bytesToString());
+      }
+    } else {
+      if (kDebugMode) {
+        print(response.reasonPhrase);
+      }
+    }
+  }
+
   void publish() {
     register = Register(
       teamName: teamName.value,
@@ -43,22 +89,19 @@ class RegisterController extends GetxController {
       eventInfo: eventInfo,
     );
 
-Get.snackbar(
-              "Success",
-               "${register.teamName} registered Successfully",
-               icon: const Icon(Icons.check_circle_outline_rounded, color: Colors.white),
-               snackPosition: SnackPosition.BOTTOM,
-               backgroundColor: Colors.green,
-               borderRadius: 20,
-               margin: const EdgeInsets.all(15),
-               colorText: Colors.white,
-               duration: const Duration(seconds: 4),
-               isDismissible: true,
-               dismissDirection: DismissDirection.down,
-               forwardAnimationCurve: Curves.easeOutBack,
-
-               );
-
-   
+    Get.snackbar(
+      "Success",
+      "${register.teamName} registered Successfully",
+      icon: const Icon(Icons.check_circle_outline_rounded, color: Colors.white),
+      snackPosition: SnackPosition.BOTTOM,
+      backgroundColor: Colors.green,
+      borderRadius: 20,
+      margin: const EdgeInsets.all(15),
+      colorText: Colors.white,
+      duration: const Duration(seconds: 4),
+      isDismissible: true,
+      dismissDirection: DismissDirection.down,
+      forwardAnimationCurve: Curves.easeOutBack,
+    );
   }
 }
--- lib/view/screen/register.dart
+++ lib/view/screen/register.dart
@@ -1,51 +1,140 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:get/instance_manager.dart';
+import 'package:techathon/controller/register_controller.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
-
+  // const RegisterScreen({Key? key}) : super(key: key);
+  RegisterController registerController = Get.put(RegisterController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              width: 375.w,
-              height: 108.h,
-              padding: EdgeInsets.only(top: 20.h),
-              alignment: Alignment.centerLeft,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/bg.png"),
-                  fit: BoxFit.cover,
-                )
-              ),
-              child: Row(
-                children: [
-                  IconButton(onPressed: (){
+        body: Container(
+            child: Column(
+      children: [
+        Container(
+          width: 375.w,
+          height: 108.h,
+          padding: EdgeInsets.only(top: 20.h),
+          alignment: Alignment.centerLeft,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/bg.png"),
+            fit: BoxFit.cover,
+          )),
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () {
                     Get.back();
-                  }, icon: 
-                  Icon(Icons.arrow_back_ios_rounded, color: Colors.white, size: 24.sp,)
-                  ),
-                  Text(
-                    "Register",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 27.sp,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  
-                ],
+                  },
+                  icon: Icon(
+                    Icons.arrow_back_ios_rounded,
+                    color: Colors.white,
+                    size: 24.sp,
+                  )),
+              Text(
+                "Register",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 27.sp,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            )
-          ],
+            ],
+          ),
+        ),
+        // Text("data")
+        Container(
+          margin: EdgeInsets.only(top: 23.h),
+          child: Column(
+              children: [ImagePicker(registerController: registerController)]),
         )
-      )
+      ],
+    )));
+  }
+}
+
+class ImagePicker extends StatelessWidget {
+  const ImagePicker({
+    Key? key,
+    required this.registerController,
+  }) : super(key: key);
+
+  final RegisterController registerController;
+  void showModal(context) {
+    showModalBottomSheet<void>(
+      // context and builder are
+      // required properties in this widget
+      context: context,
+      builder: (BuildContext context) {
+        // we set up a container inside which
+        // we create center column and display text
+
+        // Returning SizedBox instead of a Container
+        return SizedBox(
+          height: 100,
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextButton.icon(
+                    onPressed: () {
+                      registerController.getFromCamera();
+                    },
+                    icon: const Icon(Icons.camera),
+                    label: const Text("Camera")),
+                TextButton.icon(
+                    onPressed: () => registerController.getFromGallery(),
+                    icon: const Icon(Icons.image),
+                    label: const Text("Gallery")),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Stack(
+        children: [
+    registerController.imagePath.value !=""?
+          Image.file(
+            File(registerController.imagePath.value),
+            width: 375.w,
+            height: 200.h,
+            fit: BoxFit.cover,
+          ): Image.asset('assets/signup.png',
+          height: 94.h,
+          width: 94.w,
+          fit: BoxFit.cover,
+          ) ,
+    Container(
+      height: 94.h,
+      width: 94.w,
+    color: Color.fromARGB(151, 94, 92, 92),
+      alignment: Alignment.center,
+      child: IconButton(
+      onPressed: () {
+        showModal(context);
+      },
+      icon: Icon(
+        Icons.edit_outlined,
+        color: Colors.white,
+        size: 30.sp,
+      ),
+      ),
+    )
+    
+    
+    
+        ],
+      ),
     );
   }
 }
